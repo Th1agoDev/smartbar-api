7ded1b598d1663aff1e42d5920ec1fe91182b1a0
Th1agoDev <thiago1limabezerra@gmail.com>
2024-03-20 18:30:07 -0300

    registering category

--- src/services/category/CreateCategoryService.ts
+++ src/services/category/CreateCategoryService.ts
@@ -1,8 +1,28 @@
+import { Category } from "@prisma/client";
 import prismaClient from "../../prisma";
 
+interface categoryRequest{
+    name:string;
+}
+
 export class CreateCategoryService{
-    async execute(){
-        return{ok:true}
+    async execute({ name}:categoryRequest){
+
+        if(name ===''){
+            throw new Error('Name incorrect')
+        }
+        
+        const category = await prismaClient.category.create({
+            data:{
+               name:name,
+
+            },
+            select:{
+                id:true,
+                name:true,
+            }
+        })
+        return category;
     }
 }
 
